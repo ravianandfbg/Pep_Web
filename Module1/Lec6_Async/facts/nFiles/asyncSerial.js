4467d58a167cc25155de69ae3064e30f8c6b1572
Ravi Anand <ravianandfbg@gmail.com>
2021-05-18 13:49:03 +0530

    Leardboard done

---
+++ Module1/Lec6_Async/facts/nFiles/asyncSerial.js
@@ -0,0 +1,10 @@
+let files = ["../f1.txt" , "../f2.txt" , "../f3.txt"];
+const fs = require("fs");
+
+//F1kaData => f2KaData => f3KaData
+//async code
+
+
+// not possible with while loop , do while , for loop , for each
+
+// try recursive solution ?
