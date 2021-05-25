9792d8e4f1f092a8179b783c225f1350e6e5f2e1
Ravi Anand <ravianandfbg@gmail.com>
2021-05-25 13:57:06 +0530

    open warm up challenges selector

--- Module1/Lec8_Automation/login_puppeteer.js
+++ Module1/Lec8_Automation/login_puppeteer.js
@@ -1,9 +1,9 @@
 //npm install puppeteer => install in package.json
 
 const puppeteer = require('puppeteer')
-const id = 'celic91626@kindbest.com';
-const password = '123456';
-let tab;
+const id = 'rilaj74184@brayy.com'
+const password = '123456'
+let tab
 
 // puppeteer has promisfied functions
 
@@ -12,8 +12,8 @@ let tab;
 let browserOpenPromise = puppeteer.launch({
   headless: false,
   defaultViewport: null,
-  args: ["--start-maximized"],
-});
+  args: ['--start-maximized']
+})
 
 // Promise<Pending>
 browserOpenPromise
@@ -28,23 +28,41 @@ browserOpenPromise
 
   //type email id
   .then(function () {
-    return tab.type("#input-1" , id); 
+    return tab.type('#input-1', id)
 
     // return tab.type("#input-1" , id , {delay : 100}); //both are same , only "delay" function is used to slow the type speed
-
   })
 
   //type password
   .then(function () {
-        return tab.type("#input-2" , password);       
+    return tab.type('#input-2', password)
     //   return tab.type("#input-2" , password , {delay : 100}); //both are same , only "delay" function is used to slow the type speed
-
   })
 
   //click login button
-  .then (function () {
-      return tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+  .then(function () {
+    return tab.click(
+      '.ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled'
+    )
   })
-  .then (function () {
-      console.log("logged in !!");
+
+  //   wait for promise pending ("#base-card-1-link") and then it will click on interview kit
+  //  wait to open "interview kit" selector and then click
+  .then(function () {
+    return tab.waitForSelector('#base-card-1-link', { visible : true })
   })
+  //open "interviw kit"
+  .then(function () {
+    return tab.click('#base-card-1-link')
+  })
+
+  // wait to open "warm up challenges" selector and then click
+  .then(function () {
+    return tab.waitForSelector('a[data-attr1="warmup"]', { visible : true })
+  })
+  //open "warm up challenges"
+  .then(function () {
+      return tab.click('a[data-attr1="warmup"]');
+  });
+
+  
