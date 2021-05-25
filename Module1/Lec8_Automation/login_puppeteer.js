38df6a3cdd9e735376fd13eb8598ea39b5ec8382
Ravi Anand <ravianandfbg@gmail.com>
2021-05-25 12:44:44 +0530

    log in hackerRank

--- Module1/Lec8_Automation/login_puppeteer.js
+++ Module1/Lec8_Automation/login_puppeteer.js
@@ -1,23 +1,50 @@
 //npm install puppeteer => install in package.json
 
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer')
+const id = 'celic91626@kindbest.com';
+const password = '123456';
+let tab;
 
 // puppeteer has promisfied functions
 
 // by default headless = true
 
-let browserOpenPromise = puppeteer.launch({ headless: false });
-console.log(browserOpenPromise);
-// Promise<Pending>
-browserOpenPromise.then( function(browser){
-    console.log("browser is opened !");
-    return browser.pages();
-})
-.then(function(pages){
-    let tab = pages[0];
-    return tab.goto("https://www.google.com");
-})
-.then(function(){
-    console.log("On google homepage !!");
+let browserOpenPromise = puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+  args: ['--start-maximized']
 })
 
+// Promise<Pending>
+browserOpenPromise
+  .then(function (browser) {
+    console.log('browser is opened !')
+    return browser.pages()
+  })
+  .then(function (pages) {
+    tab = pages[0]
+    return tab.goto('https://www.hackerrank.com/auth/login')
+  })
+
+  //type email id
+  .then(function () {
+    return tab.type("#input-1" , id); 
+
+    // return tab.type("#input-1" , id , {delay : 100}); //both are same , only "delay" function is used to slow the type speed
+
+  })
+
+  //type password
+  .then(function () {
+        return tab.type("#input-2" , password);       
+    //   return tab.type("#input-2" , password , {delay : 100}); //both are same , only "delay" function is used to slow the type speed
+
+  })
+
+  //click login button
+  .then (function () {
+      return tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
+  })
+  .then (function () {
+      console.log("logged in !!");
+  })
