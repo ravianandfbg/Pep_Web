b0eb73fbe193800ed38b27903173b9d3fc382356
Ravi Anand <ravianandfbg@gmail.com>
2021-06-22 21:35:43 +0530

    tool options of pen and eraser

--- Module2/White Board/activity/script.js
+++ Module2/White Board/activity/script.js
@@ -6,6 +6,7 @@ canvas.height = window.innerHeight - 100;
 window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight - 100;
+  drawLinesFromDB();
 });
 
 // canvas drawing gets erased on window resize ???
@@ -13,54 +14,55 @@ window.addEventListener("resize", function () {
 // a context object which provides fun for 2d drawing
 let ctx = canvas.getContext("2d");
 
-
 let linesDB = [];
 let redoLinesDB = [];
 let isPenDown = false;
 let line = [];
 
-canvas.addEventListener("mousedown" , function(e){
-    if(redoLinesDB.length){
-        redoLinesDB = [];
-    }
-    console.log("Inside mouse down");
-    isPenDown = true;
+canvas.addEventListener("mousedown", function (e) {
+  if (redoLinesDB.length) {
+    redoLinesDB = [];
+  }
+  console.log("Inside mouse down");
+  isPenDown = true;
+  let x = e.clientX;
+  let y = e.clientY - 100;
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  let pointObject = {
+    x: x,
+    y: y,
+    type: "md",
+    lineWidth: ctx.lineWidth,
+    strokeStyle: ctx.strokeStyle,
+  };
+  line.push(pointObject);
+});
+
+canvas.addEventListener("mousemove", function (e) {
+  if (isPenDown) {
+    console.log("Inside mousemove");
     let x = e.clientX;
-    let y = e.clientY-100;
-    ctx.beginPath();
-    ctx.moveTo(x , y);
+    let y = e.clientY - 100;
+    ctx.lineTo(x, y);
+    ctx.stroke();
 
     let pointObject = {
-        x:x , 
-        y:y , 
-        type:"md"
-    }
+      x: x,
+      y: y,
+      type: "mm",
+    };
     line.push(pointObject);
-})
+  }
+});
 
-canvas.addEventListener("mousemove" , function(e){
-    if(isPenDown){
-        console.log("Inside mousemove")
-        let x = e.clientX;
-        let y = e.clientY-100;
-        ctx.lineTo(x , y);
-        ctx.stroke();
+canvas.addEventListener("mouseup", function (e) {
+  console.log("mouseup");
+  isPenDown = false;
 
-        let pointObject = {
-            x:x , 
-            y:y , 
-            type:"mm"
-        }
-        line.push(pointObject);
-    }
-})
+  linesDB.push(line);
+  line = [];
 
-canvas.addEventListener("mouseup" , function(e){
-    console.log("mouseup");
-    isPenDown = false;
-
-    linesDB.push(line);
-    line = [];
-
-    console.log(linesDB);
-})
+  console.log(linesDB);
+});
