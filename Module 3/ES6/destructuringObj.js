501af951055df8bf0efd8029b22a0efc30d90da3
Ravi Anand <ravianandfbg@gmail.com>
2021-08-02 20:20:18 +0530

    destructuring object intro

---
+++ Module 3/ES6/destructuringObj.js
@@ -0,0 +1,11 @@
+let address = {
+      city : "Delhi",
+      region : "pitampura",
+      pin : 12345
+};
+
+let {region , pin , city} = address;
+
+console.log(city);
+console.log(region);
+console.log(pin);
