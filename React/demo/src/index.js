4287c21b0c17cf727c22e3e046df78fd2df3d42b
Ravi Anand <ravianandfbg@gmail.com>
2021-08-03 18:30:08 +0530

    index

--- React/demo/src/index.js
+++ React/demo/src/index.js
@@ -1,5 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+
+//Libraries
+
+import React from "react"; // Creation Logic
+import ReactDOM from "react-dom"; // render Logic
+
+// App is a component we have import
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+                <App />, // ek component
+                document.getElementById("root") // aur ek jagah jeha us component ko dikhana hai
+                ); 
