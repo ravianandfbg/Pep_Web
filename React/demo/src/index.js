b1c601c37c481fb40e2b3330118c3c1871f5e3a1
Ravi Anand <ravianandfbg@gmail.com>
2021-08-03 20:41:28 +0530

    input

--- React/demo/src/index.js
+++ React/demo/src/index.js
@@ -5,25 +5,25 @@ import React from "react"; // Creation Logic
 import ReactDOM from "react-dom"; // render Logic
 
 // App is a component we have import
-// import App from "./App";
+import App from "./App";
 
-import MyComp from "./MyComp";
+// import MyComp from "./MyComp";
 
 ReactDOM.render(
-            //     <App />, // ek component
+                <App />, // ek component
             //     <MyComp />,
 
-            <div>
-                  <MyComp />
-                  <hr />
-                  <MyComp />
-                  <hr />
-                  <MyComp />
-                  <hr />
-                  <MyComp />
-                  <hr />
-                  <MyComp />
-            </div>,
+            // <div>
+            //       <MyComp />
+            //       <hr />
+            //       <MyComp />
+            //       <hr />
+            //       <MyComp />
+            //       <hr />
+            //       <MyComp />
+            //       <hr />
+            //       <MyComp />
+            // </div>,
 
                 document.querySelector("#root") // aur ek jagah jeha us component ko dikhana hai
                 ); 
