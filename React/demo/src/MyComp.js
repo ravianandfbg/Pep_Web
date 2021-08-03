d52812d8e1f93bfeb0e44a98049e859cffad2ce0
Ravi Anand <ravianandfbg@gmail.com>
2021-08-03 20:02:03 +0530

    increment state

--- React/demo/src/MyComp.js
+++ React/demo/src/MyComp.js
@@ -2,13 +2,24 @@ import React from "react";
 
 class MyComp extends React.Component{
       state = {
-            someNumber : 2,
+            someNumber : 0,
       };
 
       render = () => {
+
+            console.log("Render function is executed");
+
             return (
                   <div>
-                        <h1> {this.state.someNumber}</h1>
+                        <button
+                        onClick = { () => {
+                              this.setState({someNumber : this.state.someNumber + 1});
+                        } }
+                        >
+                              increment
+
+                        </button>
+                        <h1> {this.state.someNumber} </h1>
                   </div>
             );
       };
