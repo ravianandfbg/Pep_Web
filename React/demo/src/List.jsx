c030935ae5e545699ccc00b21e3cfb180e31e4c0
Ravi Anand <ravianandfbg@gmail.com>
2021-08-04 18:52:20 +0530

    List and props

---
+++ React/demo/src/List.jsx
@@ -0,0 +1,26 @@
+let List = (props) => { // props : we can give datausing props ; App(parent component) -------> List(child component)
+      return (
+        <ul>
+          {props.tasks.map((el) => {
+            return (
+              <li>
+                {el}{" "}
+                {/* <button
+                  onClick={() => {
+                    let currTaskArr = this.state.tasks;
+                    let filteredArr = currTaskArr.filter((element) => {
+                      return element != el;
+                    });
+                    this.setState({ tasks: filteredArr });
+                  }}
+                >
+                  Delete
+                </button> */}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    };
+    
+    export default List;
